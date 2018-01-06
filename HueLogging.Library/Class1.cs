3f8b36a47f319665d336786f3dac77cd951c600c
Emmanuel Quagraine <eman_the_man@hotmail.com>
2018-01-05 19:11:38 -0600

    inital commit

---
+++ HueLogging.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HueLogging.Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ HueLogging.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HueLogging.Models
+{
+    public class Class1
+    {
+    }
+}
