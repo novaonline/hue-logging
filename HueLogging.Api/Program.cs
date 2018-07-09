1964ce178cc4fbf298c3a22369b66960055b64e1
Your Name <eman_the_man@hotmail.com>
2018-07-08 20:22:34 -0500

    added angular web application

--- HueLogging.Api/Program.cs
+++ HueLogging.Api/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,16 +11,15 @@ using Microsoft.Extensions.Logging;
 
 namespace HueLogging.Api
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build();
+			host.Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+	}
 }
